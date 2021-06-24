75e8c2f37d1e3212f0e0247f7601e20f66210120
Marko Mustač <mmustac@tria.hr>
2021-06-24 12:33:16 +0200

    init

---
+++ src/main/java/com/example/deployspringtest/DeploySpringTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.deployspringtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploySpringTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeploySpringTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/deployspringtest/DeploySpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deployspringtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeploySpringTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
